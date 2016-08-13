08dcd300104750dba3e338b5c92549773ef262e0
Rafael García <rafael@mo2o.com>
2016-08-13 12:25:38 +0200

    feat: Add first routes.

--- app/scripts/routes/curriculumVitae.js
+++ app/scripts/routes/curriculumVitae.js
@@ -3,10 +3,33 @@
 CurriculumVitae.Routers = CurriculumVitae.Routers || {};
 
 (function () {
-  'use strict';
+    'use strict';
 
-  CurriculumVitae.Routers.CurriculumVitae = Backbone.Router.extend({
+    CurriculumVitae.Routers.CurriculumVitae = Backbone.Router.extend({
+        routes: {
+            '': 'index',
+            'profile': 'profile',           // #profile
+            'job/:jobId': 'job',            // #mo2o
+            'job/:jobId/:projectId': 'job'  // #mo2o/iberostar
+        },
+        index: function () {
+            // debugger;
+        },
+        profile: function () {
+            // debugger;
+        },
+        job: function (jobId, projectId) {
+            // debugger;
+        }
+    });
 
-  });
+    CurriculumVitae.Routers.AppRouter = new CurriculumVitae.Routers.CurriculumVitae;
+
+    CurriculumVitae.Routers.AppRouter.on('route:index', function (actions) {
+        // debugger;
+        // alert(actions);
+    });
+
+    Backbone.history.start();
 
 })();
